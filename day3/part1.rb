1ca519171407029476b44f9a6d72d2c613b1b1cc
Szymon Kociuba <Szymon_Kociuba@epam.com>
2020-12-04 11:38:02 +0100

    tab size

--- day3/part1.rb
+++ day3/part1.rb
@@ -5,12 +5,11 @@ class Day3
 		@input = input
 	end
 
-
 	def caluclateObstacles(start_position, sloopes)
 		@input.drop(1).count do |line| 
-		    tree = line[start_position % line.length] == '#'
-		    start_position += sloopes
-		    tree
+			tree = line[start_position % line.length] == '#'
+			start_position += sloopes
+			tree
 		end
 	end
 
