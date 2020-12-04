d0bd63557d3a11f9a8bbf203f28cd42bf4ee7bc9
Szymon Kociuba <Szymon_Kociuba@epam.com>
2020-12-04 12:16:03 +0100

    day3 part2 solution

--- day3/part1.rb
+++ day3/part1.rb
@@ -5,11 +5,19 @@ class Day3
 		@input = input
 	end
 
-	def caluclateObstacles(start_position, slopes)
-		@input.drop(1).count do |line| 
-			tree = line[start_position % line.length] == '#'
-			start_position += slopes
-			tree
+	def caluclateObstacles(start_position, slopes, down = 1)
+		@input.drop(down).each_with_index.count do |line, idx|
+			unless down != 1
+				tree = line[start_position % line.length] == '#'
+				start_position += slopes
+				tree		
+			else 
+				if idx.even?
+					tree = line[start_position % line.length] == '#'
+					start_position += slopes
+					tree
+				end
+			end 
 		end
 	end
 
--- day3/solution.rb
+++ day3/solution.rb
@@ -6,4 +6,15 @@ input = File.read("input.txt").split("\n")
 
 d = Day3.new(input)
 
-p d.caluclateObstacles(3, 3)
+# part1
+# p d.caluclateObstacles(3, 3)
+
+res = 1
+
+['1;1','3;1','5;1','7;1','1;2'].each do |e|
+	right, down = e.split(";")
+	trees = d.caluclateObstacles(right.to_i, right.to_i, down.to_i)
+	res *= trees
+end
+
+p res
